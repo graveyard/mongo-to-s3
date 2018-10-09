bd26dfbe7e95d292074735f5846cc3acffa323c7
Anthony Fumagalli <anthony.fumagalli@clever.com>
2018-10-09 14:42:33 -0700

    Test new functionality

--- main_test.go
+++ main_test.go
@@ -10,35 +10,30 @@ import (
 )
 
 func TestTableRetrieval(t *testing.T) {
-	gearmanAdminURL = "" // Hacky way to avoid posting a job
-
 	table1 := config.Table{Destination: "schools_dest", Source: "schools_source"}
 	table2 := config.Table{Destination: "teachers_dest", Source: "teachers_source"}
 	table3 := config.Table{Destination: "students_dest", Source: "students_source"}
-	testConfig1 := config.Config{"schools": table1, "teachers": table2, "students": table3}
+	testConfig := config.Config{"schools": table1, "teachers": table2, "students": table3}
 
-	// Test regular select source tables, in order specified
-	tables, err := getTablesFromConf("students_source,schools_source", testConfig1)
+	// Test regular select source table
+	table, err := getTableFromConf("students_source", testConfig)
 	assert.NoError(t, err)
-	assert.Equal(t, len(tables), 2)
-	assert.Equal(t, tables[0].Destination, "students_dest")
-	assert.Equal(t, tables[1].Destination, "schools_dest")
+	assert.Equal(t, table.Destination, "students_dest")
 
-	// Return error if none match
-	tables, err = getTablesFromConf("foo", testConfig1)
+	// Return error if no match
+	table, err = getTableFromConf("foo", testConfig)
 	assert.Error(t, err)
 
-	// Get all tables if none specified
-	testConfig2 := config.Config{"America": config.Table{Destination: "Hawaii"}}
-	tables, err = getTablesFromConf("", testConfig2)
-	assert.NoError(t, err)
-	assert.Equal(t, len(tables), 1)
-	assert.Equal(t, tables[0].Destination, "Hawaii")
+	// Return error if trying to get multiple collections
+	table, err = getTableFromConf("students_source,schools_source", testConfig)
+	assert.Error(t, err)
+
+	// Return error if no collection specified
+	table, err = getTableFromConf("", testConfig)
+	assert.Error(t, err)
 }
 
 func TestCreateManifest(t *testing.T) {
-	gearmanAdminURL = "" // Hacky way to avoid posting a job
-
 	reader, err := createManifest("bucket", []string{"foo", "bar"})
 	assert.NoError(t, err)
 	expectedManifest := &Manifest{
