21ad3218efd93a3a95870e5e33645fcabad87d6b
Benji Stein <benji.stein@clever.com>
2019-01-31 15:44:22 -0800

    Put s3 data into subfolders.

--- main.go
+++ main.go
@@ -123,7 +123,11 @@ func formatFilename(timestamp, collectionName, fileIndex, extension string) stri
 		// add underscore for readability
 		fileIndex = fmt.Sprintf("_%s", fileIndex)
 	}
-	return fmt.Sprintf("mongo_%s_%s%s%s", collectionName, timestamp, fileIndex, extension)
+	t, _ := time.Parse(time.RFC3339, timestamp)
+	filePath := fmt.Sprintf("mongo/%s/_data_timestamp_year=%02d/_data_timestamp_month=%02d/_data_timestamp_day=%02d/",
+		collectionName, t.Year(), int(t.Month()), t.Day())
+	fileName := fmt.Sprintf("mongo_%s_%s%s%s", collectionName, timestamp, fileIndex, extension)
+	return fileName + filePath
 }
 
 func exportData(source optimus.Table, table config.Table, sink optimus.Sink, timestamp string) (int, error) {
